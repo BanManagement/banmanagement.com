105a166f9aa7cb9722994725415a078364a9a92b
James Mortemore <jamesmortemore@gmail.com>
2020-01-03 11:38:35 +0000

    feat: add sponge to homepage

--- pages/index.js
+++ pages/index.js
@@ -35,20 +35,35 @@ function Page ({ isMobileFromSSR }) {
       <Segment style={{ padding: '2em 0em', clear: 'both' }} vertical>
         <Grid container divided stretched stackable centered columns='equal' verticalAlign='middle'>
           <Grid.Row>
+            <Header>Server Plugins</Header>
+          </Grid.Row>
+          <Grid.Row>
             <Grid.Column textAlign='center'>
               <Image src='/static/images/bukkit-logo.png' size='tiny' centered alt='Bukkit Logo' />
-              <Header as='h3' style={{ fontSize: '1.5em' }}>BanManager</Header>
-              <Header.Content>The original Java Minecraft Bukkit plugin</Header.Content>
-              <Header.Content>
+              <Header as='h3' style={{ fontSize: '1.5em' }}>Bukkit</Header>
+              <Header.Content style={{ marginTop: '1em' }}>
                 <Button size='big' icon='download' as='a' href='https://dev.bukkit.org/projects/ban-management' aria-label='Download BanManager' />
                 <Button size='big' icon='github' as='a' href='https://github.com/BanManagement/BanManager' aria-label='View BanManager Source' />
               </Header.Content>
             </Grid.Column>
             <Grid.Column textAlign='center'>
+              <Image src='/static/images/sponge-logo.svg' size='tiny' centered alt='Sponge Logo' />
+              <Header as='h3' style={{ fontSize: '1.5em' }}>Sponge</Header>
+              <Header.Content style={{ marginTop: '1em' }}>
+                <Button size='big' icon='download' as='a' href='https://ore.spongepowered.org/confuser/BanManager' aria-label='Download BanManager' />
+                <Button size='big' icon='github' as='a' href='https://github.com/BanManagement/BanManager' aria-label='View BanManager Source' />
+              </Header.Content>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Header>Web Applications</Header>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column textAlign='center'>
               <Image src='/static/images/graphql-logo.png' size='tiny' centered alt='GraphQL Logo' />
               <Header as='h3' style={{ fontSize: '1.5em' }}>BanManager-WebAPI</Header>
               <Header.Content>A Node.js GraphQL based API to manage all your data</Header.Content>
-              <Header.Content>
+              <Header.Content style={{ marginTop: '1em' }}>
                 <Button size='big' icon='download' as='a' href='https://github.com/BanManagement/BanManager-WebAPI' aria-label='Download BanManager-WebAPI' />
                 <Button size='big' icon='github' as='a' href='https://github.com/BanManagement/BanManager-WebAPI' aria-label='View BanManagerWebAPI Source' />
               </Header.Content>
@@ -57,7 +72,7 @@ function Page ({ isMobileFromSSR }) {
               <Image src='/static/images/react-logo.png' size='tiny' centered alt='React Logo' />
               <Header as='h3' style={{ fontSize: '1.5em' }}>BanManager-WebUI</Header>
               <Header.Content>A Node.js React based UI</Header.Content>
-              <Header.Content>
+              <Header.Content style={{ marginTop: '1em' }}>
                 <Button size='big' icon='download' as='a' href='https://github.com/BanManagement/BanManager-WebUI' aria-label='Download BanManager-WebUI' />
                 <Button size='big' icon='github' as='a' href='https://github.com/BanManagement/BanManager-WebUI' aria-label='View BanManager-WebUI Source' />
               </Header.Content>
