bb58c863775357aea191cd4fdfb89fc7ab752447
James Mortemore <jamesmortemore@gmail.com>
2020-01-10 16:51:11 +0000

    feat: added better setup and config instructions

---
+++ components/ConfigList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { List } from 'semantic-ui-react'
+
+export default class ConfigList extends React.Component {
+  renderItems(options) {
+    return Object.entries(options).map(([ header, description ]) => {
+      if (typeof description === 'string' || description.props) {
+        return (
+          <List.Item>
+            <List.Icon />
+            <List.Content>
+              <List.Header>{header}</List.Header>
+              <List.Description>{description}</List.Description>
+            </List.Content>
+          </List.Item>
+        )
+      } else {
+        return (
+          <List.Item>
+            <List.List>
+              <List.Item>
+                <List.Icon />
+                <List.Content>
+                  <List.Header>{header}</List.Header>
+                  <List.List>{this.renderItems(description)}</List.List>
+                </List.Content>
+              </List.Item>
+            </List.List>
+          </List.Item>
+        )
+      }
+    })
+  }
+
+  render() {
+    return <List>{this.renderItems(this.props.options)}</List>
+  }
+}
--- pages/faq.js
+++ pages/faq.js
@@ -1,16 +1,27 @@
-import { Container, Segment } from 'semantic-ui-react'
+import { Container, Header, Segment } from 'semantic-ui-react'
 import DefaultLayout from '../components/DefaultLayout'
 import { getInitialProps } from '../utils'
 
 function Page ({ isMobileFromSSR }) {
   return (
-    <DefaultLayout title='Support' isMobileFromSSR={isMobileFromSSR}>
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Segment vertical>
-          <Container text>
-          Coming Soon
-          </Container>
-        </Segment>
+    <DefaultLayout title='FAQ' isMobileFromSSR={isMobileFromSSR}>
+      <Segment vertical>
+        <Container text>
+          <Header as='h1'>FAQ</Header>
+
+          <Header as='h2' id='startup-issues'>Startup Issues</Header>
+          <Header as='h4'>Q: BanManager is showing as red in /plugins</Header>
+          <p>Please ensure you have configured the plugin correctly and check either your server log or console for more information</p>
+          <Header as='h4'>Q: When trying to /ban a player it displays /ban {"<player> <reason>"}</Header>
+
+          <p>The plugin is not enabled, please check your server startup log for more information</p>
+          <Header as='h4'>java.sql.SQLSyntaxErrorException: Table 'bm_players' doesn't exist</Header>
+          <p>This occurs when the wrong JDBC driver is in use. If you are using MariaDB, ensure <code>storageType</code> is set to <code>mariadb</code>.</p>
+
+          <Header as='h2' id='sync'>Cross-Server Sync</Header>
+          <Header as='h4'>Q: I punished a player but it doesn't appear to have synced across all servers</Header>
+          <p>Each server polls for data changes including new punishments. Open your <code>schedules.yml</code> file and check your timings. By default local punishments are checked every 30 seconds and global punishments every 2 minutes. Checking more frequently will increase database usage.</p>
+        </Container>
       </Segment>
     </DefaultLayout>)
 }
