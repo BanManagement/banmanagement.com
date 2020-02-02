c71dfaa6b4a12ce4b8d0875ba43ab85647ec22da
James Mortemore <jamesmortemore@gmail.com>
2020-02-02 01:56:24 +0000

    feat: add links in  support page

--- pages/support.js
+++ pages/support.js
@@ -1,16 +1,21 @@
-import { Container, Segment } from 'semantic-ui-react'
+import { Container, Header, Segment } from 'semantic-ui-react'
 import DefaultLayout from '../components/DefaultLayout'
 import { getInitialProps } from '../utils'
 
 function Page ({ isMobileFromSSR }) {
   return (
     <DefaultLayout title='Support' isMobileFromSSR={isMobileFromSSR}>
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Segment vertical>
-          <Container text>
-          Coming Soon
-          </Container>
-        </Segment>
+      <Segment vertical>
+        <Container text>
+          <Header as='h1'>Support</Header>
+          <p>Checkout the <a href='/faq'>FAQ</a> page for the most commonly asked questions</p>
+
+          <Header as='h2'>Join Discord</Header>
+          <p>Having trouble setting something up? Have questions? <a href='https://discord.gg/59bsgZB'>Join the Discord server</a></p>
+
+          <Header as='h2'>Discuss on GitHub</Header>
+          <p>Found a bug, or want to request a new feature? You can join the discussion on <a href='https://github.com/BanManagement/BanManager/issues'>GitHub</a></p>
+        </Container>
       </Segment>
     </DefaultLayout>)
 }
