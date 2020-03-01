1828741ed588d5287415227a33b761f8b5602f7a
James Mortemore <jamesmortemore@gmail.com>
2020-03-01 12:27:38 +0000

    feat: more faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,4 +1,4 @@
-import { Container, Header, Segment } from 'semantic-ui-react'
+import { Container, Header, List, Segment } from 'semantic-ui-react'
 import DefaultLayout from '../components/DefaultLayout'
 import { getInitialProps } from '../utils'
 
@@ -12,11 +12,26 @@ function Page ({ isMobileFromSSR }) {
           <Header as='h2' id='startup-issues'>Startup Issues</Header>
           <Header as='h4'>Q: BanManager is showing as red in /plugins</Header>
           <p>Please ensure you have configured the plugin correctly and check either your server log or console for more information</p>
-          <Header as='h4'>Q: When trying to /ban a player it displays /ban {'<player> <reason>'}</Header>
 
+          <Header as='h4'>Q: When trying to /ban a player it displays /ban {'<player> <reason>'}</Header>
           <p>The plugin is not enabled, please check your server startup log for more information</p>
+
+          <Header as='h4'>Q: BanManager is unable to connect to the database</Header>
+          <p>This can be caused by a number of issues, below lists the most common. If these do not resolve your issue, feel free to request <a href='/support'>Support</a>.</p>
+          <List bulleted>
+            <List.Item>Ensure you have created the database. Verify this by logging in with the user you are connecting with (via mysql -p -u) and running <code>SHOW DATABASES;</code>. If the database is not contained in the results, it either has not been created or the user does not have permission to access it.</List.Item>
+            <List.Item>If you are connecting to the database from another server, you need to explicitly grant access to it for the remote ip address as follows: <code>GRANT ALL PRIVILEGES ON dbname.* TO username@'IP' IDENTIFIED BY 'password';</code> replacing dbname, username and password with your respective details.Then run <code>FLUSH PRIVILEGES;</code> to ensure they are updated.</List.Item>
+            <List.Item>If after verifing the aforementioned checks the plugin is still unable to connect, check the MySQL/MariaDB error logs.</List.Item>
+          </List>
+
+          <Header as='h4'>Q: I'd like to use BanManager with Tekkit Classic, which version is compatible?</Header>
+          <p>v2.x versions of BanManager have been reported as compatible. At a mimimum you will require <a href='https://dev.bukkit.org/projects/ban-management/files/675342'>v2.2</a>, please ensure <code>useSyncChat</code> is enabled in the config. Please note the following: documentation and guides are for recent BanManager versions only, there may be bugs, older versions of BanManager are name based, not UUID meaning a change of name will evade bans for online-mode servers.</p>
+
           <Header as='h4'>java.sql.SQLSyntaxErrorException: Table 'bm_players' doesn't exist</Header>
           <p>This occurs when the wrong JDBC driver is in use. If you are using MariaDB, ensure <code>storageType</code> is set to <code>mariadb</code>.</p>
+
+          <Header as='h4'>me.confuser.banmanager.internal.mysql.cj.exceptions.CJException: Access denied for user 'root'@'localhost'</Header>
+          <p>It is not recommended you use root to connect to the database. Please use or create a non-root user to connect with.</p>
 
           <Header as='h2' id='sync'>Cross-Server Sync</Header>
           <Header as='h4'>Q: I punished a player but it doesn't appear to have synced across all servers</Header>
